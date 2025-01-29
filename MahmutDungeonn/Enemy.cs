d02e8fe57ada3577cdc5156c80fbef5c819ac604
AnilEmreAy <152299007+AnilEmreAy@users.noreply.github.com>
2025-01-29 23:43:47 +0300

    Add files via upload

---
+++ MahmutDungeonn/Enemy.cs
@@ -0,0 +1,15 @@
+﻿class Enemy
+{
+    public string Name { get; private set; }
+    public int Health { get; set; }
+    public int Attack { get; private set; }
+    public int MaxHealth { get; private set; } 
+
+    public Enemy(string name, int health, int attack)
+    {
+        Name = name;
+        Health = health;
+        MaxHealth = health; 
+        Attack = attack;
+    }
+}
